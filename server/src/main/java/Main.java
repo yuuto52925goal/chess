bbc2dfe88c08848c0730f87e3ed374d648eea6f7
yuuto52925goal <yuuto52925goal@gmail.com>
2025-02-18 11:33:50 -0700

    Make ChessServer

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -4,5 +4,16 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        try {
+            var port = 8000;
+
+        }catch (Throwable e) {
+            System.out.println("Unable to start server " + e.getMessage());
+        }
+        System.out.println("""
+                Chess Server:
+                java ServerMain <port> [sql]
+                """);
     }
 }
---
+++ server/src/main/java/server/ChessServer.java
@@ -0,0 +1,12 @@
+package server;
+
+public class ChessServer {
+
+    public ChessServer(){
+
+    }
+
+    public ChessServer run(int port){
+        return this;
+    }
+}
