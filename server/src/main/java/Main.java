b242c19c96674190c5bb62f0862a24b3ec050242
yuuto52925goal <yuuto52925goal@gmail.com>
2025-02-18 15:14:20 -0700

    Adding handler and record

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,13 +1,17 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
+
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
 
         try {
-            var port = 8000;
-
+            var port = 8080;
+            var server = new Server().run(port);
+            System.out.printf("Server started on port %d%n", server);
+            return;
         }catch (Throwable e) {
             System.out.println("Unable to start server " + e.getMessage());
         }
--- server/src/test/java/passoff/server/StandardAPITests.java
+++ server/src/test/java/passoff/server/StandardAPITests.java
@@ -1,4 +1,4 @@
-package test.java.passoff.server;
+package passoff.server;
 
 import chess.ChessGame;
 import org.junit.jupiter.api.*;
