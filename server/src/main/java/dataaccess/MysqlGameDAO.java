5c9261508ceaa578a8c39aecaa7c67fb79c98147
yuuto52925goal <yuuto52925goal@gmail.com>
2025-03-05 16:26:01 -0700

    First try of game sql

--- server/src/main/java/dataaccess/MysqlGameDAO.java
+++ server/src/main/java/dataaccess/MysqlGameDAO.java
@@ -1,7 +1,9 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.data.GameData;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -17,16 +19,65 @@ public class MysqlGameDAO extends Mysql implements GameDAO{
 
     @Override
     public ArrayList<GameData> listGames() {
-        return null;
+        String statement = "SELECT * FROM games";
+        ArrayList<GameData> games = new ArrayList<>();
+        try(var conn = DatabaseManager.getConnection()){
+            try(var stmt = conn.prepareStatement(statement)){
+                try(var rs = stmt.executeQuery()){
+                    while(rs.next()){
+                        int gameId = rs.getInt("gameId");
+                        String whiteUsername = rs.getString("whiteUsername");
+                        String blackUsername = rs.getString("blackUsername");
+                        String gameName = rs.getString("gameName");
+                        ChessGame game = rs.getObject("game", ChessGame.class);
+                        games.add(new GameData(gameId, whiteUsername, blackUsername, gameName, game));
+                    }
+                }
+            }
+        }catch (SQLException | DataAccessException e){
+            System.out.println("SQLException: " + e.getMessage());
+        }
+        return games;
     }
 
     @Override
     public int createGame(String gameName) {
-        return 0;
+        String statement = "INSERT INTO games (gameName) VALUES (?)";
+        this.executeUpdate(statement, gameName);
+        String query = "SELECT gameId FROM games WHERE gameName = ?";
+        try (var conn = DatabaseManager.getConnection()){
+            try (var stmt = conn.prepareStatement(query)){
+                try(var res = stmt.executeQuery()){
+                    if(res.next()){
+                        return res.getInt("gameId");
+                    }
+                }
+            }
+        }catch (SQLException | DataAccessException e){
+            System.out.println("SQLException: " + e.getMessage());
+        }
+        return -1;
     }
 
     @Override
     public GameData findGame(int gameId) {
+        String statement = "SELECT * FROM game WHERE gameID = ?";
+        try(var conn = DatabaseManager.getConnection();){
+            try (var stmt = conn.prepareStatement(statement)) {
+                stmt.setInt(1, gameId);
+                try (var rs = stmt.executeQuery()) {
+                    if (rs.next()) {
+                        String gameName = rs.getString("gameName");
+                        String whiteUser = rs.getString("whiteUsername");
+                        String blackUser = rs.getString("blackUsername");
+                        ChessGame game = rs.getObject("game", ChessGame.class);
+                        return new GameData(gameId, whiteUser, blackUser, gameName, game);
+                    }
+                }
+            }
+        }catch (DataAccessException| SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
@@ -37,6 +88,8 @@ public class MysqlGameDAO extends Mysql implements GameDAO{
 
     @Override
     public void deleteGames() {
+        String statement = "DELETE FROM game";
+        this.executeUpdate(statement);
 
     }
 
